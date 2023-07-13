dd05ae3fa5a5bd3b72ae20bb80b0b80eff9cf4c2
RichQ-hub <74640863+RichQ-hub@users.noreply.github.com>
2023-07-14 05:28:32 +1000

    Updated auth controller

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -5,54 +5,54 @@ import db from '../db';
 
 export async function register(req: Request, res: Response) {
     try {
-        const { 
-            name_first, 
-            email, 
-            password, 
-            birth_date, 
-            gender 
+        const {
+            name_first,
+            email,
+            password,
+            birth_date,
+            gender
         } = req.body;
-    
+
         // Check if the user exists in the database.
         const existingUser = await db.query(`
             SELECT * 
             FROM Users
             WHERE email = $1;
         `, [email]);
-    
+
         // If user already exists, return error.
         if (existingUser.rows.length !== 0) {
             return res
-            .status(401)
-            .send({ message: `User with the email ${email} already exists.`});
+                .status(401)
+                .send({ message: `User with the email ${email} already exists.` });
         }
-    
+
         // The user does not exist, so we proceed to create a new user.
-    
+
         // The salt pretty much determines how encrypted we want our password to be.
         const saltRounds = 10;
         const salt = await bcrypt.genSalt(saltRounds);
-    
+
         // Encrypt the password with the salt.
         const passwordHash = await bcrypt.hash(password, salt);
-    
+
         // Enter the new user into the database, storing the encrypted password.
-    
+
         const resultUser = await db.query(`
             INSERT INTO Users (name_first, email, password, gender, birth_date)
             VALUES ($1, $2, $3, $4, $5)
             RETURNING *;
         `, [name_first, email, passwordHash, gender, birth_date]);
-    
+
         const newUser = resultUser.rows[0];
-    
+
         // Generate our JWT token and return it to the user's client.
         const token = generateToken(newUser.id, newUser.email);
         res.json({ token });
 
     } catch (error) {
-        res.status(500).send({ 
-            message: error 
+        res.status(500).send({
+            message: error
         });
     }
 }
@@ -73,8 +73,8 @@ export async function login(req: Request, res: Response) {
 
         if (resultUser.rows.length === 0) {
             return res
-            .status(401)
-            .send({ message: `User with email \'${email}\' does not exist.` });
+                .status(401)
+                .send({ message: `User with email \'${email}\' does not exist.` });
         }
 
         const user = resultUser.rows[0];
@@ -85,8 +85,8 @@ export async function login(req: Request, res: Response) {
 
         if (!validPassword) {
             return res
-            .status(401)
-            .send({ message: 'Password incorrect.' });
+                .status(401)
+                .send({ message: 'Password incorrect.' });
         }
 
         // Generate a new token.
@@ -94,8 +94,8 @@ export async function login(req: Request, res: Response) {
         res.json({ token });
 
     } catch (error) {
-        res.status(500).send({ 
-            message: error 
+        res.status(500).send({
+            message: error
         });
     }
 }
@@ -111,10 +111,10 @@ export async function verifyToken(req: Request, res: Response) {
         // function, that immediately means that the user is authorized (the
         // token is valid). So we can just return true.
         res.json(true);
-        
+
     } catch (error) {
-        res.status(500).send({ 
-            message: error 
+        res.status(500).send({
+            message: error
         });
     }
 }

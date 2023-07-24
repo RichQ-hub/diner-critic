f3ec8e7be395df5d1fc9fb61b9ea2fcd72446eb5
RichQ-hub <74640863+RichQ-hub@users.noreply.github.com>
2023-07-24 22:46:36 +1000

    Added nav scroll functionality

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import './Navbar.css';
 
 export default function Navbar() {
+    const [isSticky, setIsSticky] = useState<boolean>(false);
+
+    /**
+     * Attach a scroll event listener using useEffect. We need useEffect since its purpose is to 
+     * interact with external systems, like API calls and in this case the DOM API, specifically
+     * the window object.
+     */
+    useEffect(() => {
+        function handleNavScroll() {
+            if (window.scrollY > 0) {
+                setIsSticky(true);
+            } else {
+                setIsSticky(false);
+            }
+            // console.log(window.scrollY)
+        }
+
+        window.addEventListener("scroll", handleNavScroll);
+
+        return (
+            window.removeEventListener("scroll", handleNavScroll)
+        )
+
+        /**
+         * How this works:
+         * We attach a scroll event listener to the window object (which is like the 
+         * global object that is the top level element to every DOM node). We essentially
+         * detect if the the user has scrolled down past the top of the page, which should
+         * transition the navbar to its sticky form.
+         */
+    }, []);
+
     return (
-        <nav className='nav'>Navbar</nav>
+        <nav 
+            className={`nav ${isSticky ? 'nav--sticky' : ''}`}
+        >
+            <h2>Nniiiiice</h2>
+        </nav>
     )
+
 }
--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import './HomePage.css';
 
 export default function HomePage() {
     return (
-        <div>HomePage</div>
+        <main>
+            <h1>Home Page</h1>
+            <div className='item'></div>
+        </main>
     )
 }
